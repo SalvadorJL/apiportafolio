863372aa2076ba96d32b731aaec8be947f166060
Salvador López <fenix_black168@hotmail.com>
2025-04-07 14:15:54 -0600

    se arreglo envio de correo externo

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -3,21 +3,27 @@ import nodemailer from 'nodemailer'
 export const enviarEmail = async (req, res) => {
   const { nombre, email, mensaje } = req.body
 
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: `Nuevo mensaje de ${nombre}`,
+    html: `
+      <p><strong>Nombre:</strong> ${nombre}</p>
+      <p><strong>Correo:</strong> ${email}</p>
+      <p><strong>Mensaje:</strong> ${mensaje}</p>
+    `
+  };
+
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // o el que uses
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
       }
     })
 
-    await transporter.sendMail({
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `Nuevo mensaje de ${nombre}`,
-      text: mensaje
-    })
+    await transporter.sendMail(mailOptions);
 
     res.status(200).json({ message: 'Correo enviado correctamente' })
   } catch (error) {
